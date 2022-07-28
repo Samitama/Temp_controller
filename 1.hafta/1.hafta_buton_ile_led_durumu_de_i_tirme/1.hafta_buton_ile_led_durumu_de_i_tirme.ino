baf33df79c07f37fa0652901584e1a94af1d42be
Samitama <90217942+Samitama@users.noreply.github.com>
2022-07-28 22:13:30 +0300

    1.hafta

---
+++ 1.hafta/1.hafta_buton_ile_led_durumu_de_i_tirme/1.hafta_buton_ile_led_durumu_de_i_tirme.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(2,OUTPUT);
+  pinMode(3,INPUT);
+}
+
+void loop() {
+  if(digitalRead(3)==HIGH){
+    digitalWrite(2,LOW);
+  }
+  else if(digitalRead(3)==LOW){
+    digitalWrite(2,HIGH);
+  }
+
+}
